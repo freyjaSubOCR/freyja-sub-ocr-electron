d23c55cc3cf39197ef815e221d329b96e0d37ce8
arition <aritionkb@gmail.com>
2020-09-11 22:36:00 -0700

    change body css

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,7 @@ table {
 body {
     display: flex;
     background: #162835;
-    padding: 40px;
     margin: 0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
 }
 
 #app {
