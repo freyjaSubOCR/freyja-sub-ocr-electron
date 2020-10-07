57bb4af55e68c56e0713d929457c427724bc788f
arition <aritionkb@gmail.com>
2020-10-06 23:08:04 -0700

    remove ununsed import

--- src/configIpc.ts
+++ src/configIpc.ts
@@ -1,7 +1,6 @@
 import { ipcMain } from 'electron'
 import logger from '@/logger'
 import Config from '@/config'
-import { config } from 'vue/types/umd'
 
 class ConfigIpc {
     static registerIPCListener(): void {
