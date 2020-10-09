acd6d06c01e56f89938c662cf2321c04a578bf91
arition <aritionkb@gmail.com>
2020-10-08 20:49:12 -0700

    reduce default batch size

--- src/config.ts
+++ src/config.ts
@@ -76,7 +76,7 @@ class Config {
     static ocrModulePath = path.resolve('./', 'models', `ocr_${Config.language}_${Config.font}.torchscript`)
     static ocrCharsPath = path.resolve('./', 'models', `ocr_${Config.language}.txt`)
     static enableCuda = true
-    static batchSize = 24
+    static batchSize = 8
     static cropTop = 0
     static cropBottom = 0
     // eslint-disable-next-line @typescript-eslint/naming-convention
